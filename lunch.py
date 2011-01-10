2b849d6b891e3c16753dd3dace76ca570b59dc8a
Matt Hickford <matt.hickford@gmail.com>
2011-01-10 22:29:34 +0000

    improve regular expression

--- lunch.py
+++ lunch.py
@@ -8,14 +8,12 @@ import commands
 import django.utils.dateformat
 import re
 
-#print "Hello lunchers! I presume most of you are going home this weekend so I will cease to send menus to this list. If you would like to receive hall menus after the end of term then subscribe to this second list http://www.srcf.ucam.org/mailman/listinfo/tchm-outofterm . Thank you."
-#print "-Matt"
-#print
-
-today = datetime.date.today()
+today = datetime.date.today()		#+datetime.timedelta(days=-5)
 #print today
 monday = today - datetime.timedelta(days=today.weekday())
 tomorrow = today + datetime.timedelta(days=1)
+
+dotw = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
 
 catering_url = "http://www.trin.cam.ac.uk/index.php?pageid=52"
 page = urllib2.urlopen(catering_url)
@@ -28,7 +26,6 @@ tomorrow2 = "%s %s" % ( str(django.utils.dateformat.format(tomorrow,"jS")), tomo
 today1 = today.strftime("%A")
 tomorrow1 = tomorrow.strftime("%A")
 
-
 menu_urls = [urlparse.urljoin(catering_url,x.parent['href']) for x in soup.findAll(text="Hall Menu")]
 print "%s %s" % (today1,today2)
 for menu_url in reversed(menu_urls):
@@ -38,32 +35,20 @@ for menu_url in reversed(menu_urls):
 	f.write(g.read())
 	f.close()
 
-	cmd = "./pdf2txt.py menu.pdf"
+	cmd = "pdf2txt.py menu.pdf"
 	status, output = commands.getstatusoutput(cmd)
 
 	output=output.strip()
 
-	filler = ["SHOULD YOU HAVE A FOOD ALLERGY PLEASE ADVISE THE CATERING MANAGER","DISHES MAY CONTAIN TRACES OF NUTS OR NUT BY-PRODUCTS","SOME OF THE FOOD PROVIDED AT THESE PREMISES CONTAINS INGREDIENTS PRODUCED FROM GENETICALLY MODIFIED SOYA BEANS AND MAIZE","FURTHER INFORMATION IS AVAILABLE FROM STAFF","Catering Department"]
+	filler = ["SHOULD YOU HAVE A FOOD ALLERGY PLEASE ADVISE THE CATERING MANAGER","DISHES MAY CONTAIN TRACES OF NUTS OR NUT BY-PRODUCTS","SOME OF THE FOOD PROVIDED AT THESE PREMISES CONTAINS INGREDIENTS PRODUCED FROM GENETICALLY MODIFIED SOYA BEANS AND MAIZE","FURTHER INFORMATION IS AVAILABLE FROM STAFF","Catering Department","MAY CONTAIN TRACES OF NUTS OR NUT BY-PRODUCTS"]
 
 	for x in filler:
 		output = output.replace(x,"")
 
-	#print output
+	if today2 in output:
 
-#	today2 = "%s %s" % ( str(django.utils.dateformat.format(today,"jS")), today.strftime("%B") )
-#	tomorrow2 = "%s %s" % ( str(django.utils.dateformat.format(tomorrow,"jS")), tomorrow.strftime("%B") )
+		pattern = "\D(?:%s)(.*?)(%s|$)" % (today2,"|".join(dotw))
 
-#	today1 = today.strftime("%A")
-#	tomorrow1 = tomorrow.strftime("%A")
-
-	if today2 not in output:
-		continue
-	else:
-#		print "%s %s" % (today1,today2)
-		
-		pattern = "\D(?:%s)(.*)(?:%s)" % (today2, tomorrow1)
-		if today.isoweekday() == 7:
-			pattern = pattern = "\D(?:.*)(?:%s)(.*)$" % (today2)
 		p = re.compile(pattern,re.IGNORECASE | re.DOTALL)
 
 		x = p.search(output)
@@ -74,3 +59,5 @@ for menu_url in reversed(menu_urls):
 		words = words.replace("\n \n \n \n","\n \n")
 		print words
 		break
+else:
+	raise Exception
